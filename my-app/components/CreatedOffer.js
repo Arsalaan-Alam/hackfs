f35bd1605a28b6c2830e185ffc1642446720e48d
Ronak Gupta <mailtoronakgupta@gmail.com>
2023-06-18 21:00:01 +0530

    link update

--- my-app/components/CreatedOffer.js
+++ my-app/components/CreatedOffer.js
@@ -38,7 +38,7 @@ const CreatedOffer= () => {
     const status = d.status === 0 ? "pending": d.status===1?"accepted":"rejected"
     const command = d.requestURI
     const visibleCommand = command?.slice(0,5) +"..."+command?.slice(-4)
-    const result = `ipfs.io/ipfs/${d.resultURI}`
+    const result = `https://ipfs.io/ipfs/${d.resultURI}`
     const list = [parseInt(d.collectionIndex),formatEther(d.deposit),visibleCommand,status,<a href={result} target='_blank'>{d.resultURI}</a>]
     return (
 <TableRow rowContent={list} key = {k}/>
